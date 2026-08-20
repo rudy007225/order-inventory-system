7eee6b3dddec5322641f039cccc27e6c313ecacc
rudy007225 <rudreshsharma379@gmail.com>
2026-08-20 14:25:32 +0530

    chore: Project setup with Spring Boot 4.0.7,Java 21,MySQL, Swagger, Security

---
+++ src/main/java/com/oms/orderinventory/OrderInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.oms.orderinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oms/orderinventory/OrderInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oms.orderinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
